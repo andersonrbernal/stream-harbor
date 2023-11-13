3b19e3b00a0df9769c1916bcf993e9751920193a
anderson <andersonbernal2017@gmail.com>
2023-11-13 18:49:04 -0400

    feat(views): add pages.auth.login view

---
+++ resources/views/pages/auth/login.blade.php
@@ -0,0 +1,14 @@
+<x-layouts.app class="bg-gray-700 flex items-center h-screen justify-center bg-auto bg-no-repeat">
+
+    <img src={{ asset('assets/images/background-image.png') }} alt="Background Image" loading="lazy"
+        class="absolute -z-50 object-cover w-full h-screen overflow-hidden">
+
+    <div class="max-w-md w-full mx-5">
+
+        <x-favicon-with-title :title="__('pages/auth/login.title')" class="w-48" />
+
+        @livewire('login-user')
+
+    </div>
+
+</x-layouts.app>
