edde8a63edb3aebb3b5d1d17ab4f5f34e8e6757f
anderson <andersonbernal2017@gmail.com>
2023-11-13 18:46:28 -0400

    chore(lang): add 'pt-br' localization for pages.auth.login route

---
+++ lang/pt-br/pages/auth/login.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    "form_title" => "Fazer login",
+    "attributes" => [
+        "email" => "email",
+        "password" => "senha",
+    ],
+    "email_input" => [
+        "label" => "Seu email",
+        "placeholder" => "Sua senha",
+    ],
+    "password_input" => [
+        "label" => "Senha",
+        "placeholder" => "********",
+    ],
+    "form_button" => 'Fazer login',
+    'form_error' => [
+        'credentials_error' => 'Email ou senha incorretos.',
+    ],
+    'form_footer' => [
+        'does_not_have_an_account' => 'Já possui uma conta?',
+        'sign_up_here' => 'Registre-se aqui',
+    ]
+];
