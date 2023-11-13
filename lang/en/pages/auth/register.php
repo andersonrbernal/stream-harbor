e3580f67743a0a6ebc76bd0a6f2e8f95059b6720
anderson <andersonbernal2017@gmail.com>
2023-11-13 12:59:42 -0400

    chore(lang): add 'en' localization for /pages/auth/register route

---
+++ lang/en/pages/auth/register.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    "form_title" => "Create Account",
+    "attributes" => [
+        "name" => "name",
+        "email" => "email",
+        "password" => "password",
+        "password_confirmation" => "password confirmation",
+        "country_id" => "country",
+    ],
+    "name_input" => [
+        "label" => "Full Name",
+        "placeholder" => "Your Name",
+    ],
+    "email_input" => [
+        "label" => "Your Email",
+        "placeholder" => "Email Address",
+    ],
+    "password_input" => [
+        "label" => "Password",
+        "placeholder" => "********",
+    ],
+    "password_confirmation_input" => [
+        "label" => "Confirm Password",
+        "placeholder" => "********",
+    ],
+    "country" => [
+        "label" => "Country",
+        "placeholder" => "Select a country",
+    ],
+    "form_button" => 'Submit',
+    'form_error' => 'Something went wrong. Please, try again later.',
+    'form_footer' => [
+        'already_have_an_account' => 'Already have an account?',
+        'login_here' => 'Login Here',
+    ]
+];
