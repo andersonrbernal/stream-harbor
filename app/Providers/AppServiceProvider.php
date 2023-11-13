b1580930f8931e750e0c4984467d566ccc8c321e
anderson <andersonbernal2017@gmail.com>
2023-11-13 12:59:08 -0400

    chore(providers): add interfaces for user and country repository
    Good for dependency injection.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Country;
+use App\Models\User;
+use App\Repositories\CountryRepository;
+use App\Repositories\Interfaces\CountryRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserRepositoryInterface::class, function () {
+            $user = new User();
+            return new UserRepository($user);
+        });
+
+        $this->app->bind(CountryRepositoryInterface::class, function () {
+            $country = new Country();
+            return new CountryRepository($country);
+        });
     }
 
     /**
