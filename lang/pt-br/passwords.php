2d2eccdf5c7265d9e1e7f5e10467d70979479381
anderson <andersonbernal2017@gmail.com>
2023-11-13 11:56:11 -0400

    chore(lang): publish brazilian portuguese language

---
+++ lang/pt-br/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma para Redefinição de Senha
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são as linhas padrão que correspondem aos motivos
+    | fornecidos pelo corretor de senhas para uma tentativa de atualização de senha que falhou,
+    | como para um token inválido ou nova senha inválida.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida.',
+    'sent' => 'Enviamos o link de redefinição de senha para o seu e-mail.',
+    'throttled' => 'Aguarde um momento antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não conseguimos encontrar um usuário com esse endereço de e-mail.",
+
+];
