4dc1fff0c7a9693c94a9a3f714caed76ff408d12
anderson <andersonbernal2017@gmail.com>
2023-11-13 11:49:00 -0400

    chore(migrations): add create_videos_table migration

---
+++ database/migrations/2023_11_05_181703_create_videos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('video_url');
+            $table->string('thumb_url');
+            $table->unsignedBigInteger('media_category_id');
+            $table->foreign('media_category_id')->references('id')->on('media_categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
