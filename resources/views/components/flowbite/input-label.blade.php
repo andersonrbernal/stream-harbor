9571b47b5bffe087d386c483aaa84276b7362d2e
anderson <andersonbernal2017@gmail.com>
2023-11-14 13:10:41 -0400

    chore(components): update tailwind styles to be compatible with dark mode

--- resources/views/components/flowbite/input-label.blade.php
+++ resources/views/components/flowbite/input-label.blade.php
@@ -1,7 +1,7 @@
 @props(['for' => null, 'error' => false])
 
 @php
-    $class = 'block mb-2 text-sm font-medium';
+    $class = 'block mb-2 text-sm font-medium text-gray-500 dark:text-gray-400';
 
     if ($error) {
         $class = $class . ' text-error-700 dark:text-error-500';
--- resources/views/components/flowbite/input.blade.php
+++ resources/views/components/flowbite/input.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $baseClass = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500';
-    $errorClass = 'bg-error-50 border border-error-500 text-error-900 placeholder-error-700 text-sm rounded-lg focus:ring-error-500 dark:bg-gray-700 focus:border-error-500 block w-full p-2.5 dark:text-error-500 dark:placeholder-error-500 dark:border-error-500';
+    $errorClass = 'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500';
 @endphp
 
 <input {{ $attributes->merge(['class' => $error ? $errorClass : $baseClass]) }} />
