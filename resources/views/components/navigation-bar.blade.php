4ede988235133fa553e62d9ddfac959c9918bd05
anderson <andersonbernal2017@gmail.com>
2023-11-17 19:31:14 -0400

    feat(livewire): add navigation bar component

---
+++ resources/views/components/navigation-bar.blade.php
@@ -0,0 +1,33 @@
+<nav class="bg-gray-200 dark:bg-gray-800 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+        <div class="flex gap-2">
+            <img src="{{ asset('favicon.svg') }}" alt="{{ __('pages/auth/register.title') }}" class="w-8 h-8">
+            <h1 class="text-lg text-gray-500 md:text-xl dark:text-gray-400">
+                {{ __('components/navigation-bar.title') }}
+            </h1>
+        </div>
+
+        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+
+            <div class="flex items-center gap-4">
+
+                <x-dark-theme-toggle />
+
+                @if ($user)
+                    @php $firstName = explode(' ', trim($user->name))[0]; @endphp
+
+                    <x-flowbite.heading heading="h4"> {{ $firstName }} </x-flowbite.heading>
+                @else
+                    <x-flowbite.link :href="route('auth.login', ['locale' => app()->getLocale()])">
+                        {{ __('pages/home/index.login_button') }}
+                    </x-flowbite.link>
+                    <x-flowbite.button-link :href="route('auth.register', ['locale' => app()->getLocale()])">
+                        {{ __('pages/home/index.sign_up_button') }}
+                    </x-flowbite.button-link>
+                @endif
+
+            </div>
+        </div>
+    </div>
+</nav>
