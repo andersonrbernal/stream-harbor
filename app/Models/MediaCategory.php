1cc102b323268ac02eeaafa07708a62cda48c7b4
anderson <andersonbernal2017@gmail.com>
2023-11-17 19:34:18 -0400

    feat(models): add media category one-to-many relationship with videos

--- app/Models/MediaCategory.php
+++ app/Models/MediaCategory.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MediaCategory extends Model
 {
@@ -17,4 +18,9 @@ class MediaCategory extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function videos(): HasMany
+    {
+        return $this->hasMany(Video::class);
+    }
 }
