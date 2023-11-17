f2c0c1b436e9d2bacba97702e5e19af3fd90793a
anderson <andersonbernal2017@gmail.com>
2023-11-17 19:24:49 -0400

    feat(interfaces): add video repository interface

---
+++ app/Repositories/Interfaces/VideoRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface VideoRepositoryInterface
+{
+    public function findAll($columns = ['*']);
+    public function findById($id);
+    public function findAllBy($columns = ['*']);
+    public function paginate($perPage = 15, $columns = ['*']);
+    public function create(array $data);
+    public function update($id, array $data);
+    public function delete($id);
+}
