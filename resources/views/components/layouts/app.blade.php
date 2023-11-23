3777bc00d09cb64e834a06c54a17ad7f74554ece
anderson <andersonbernal2017@gmail.com>
2023-11-22 22:14:42 -0400

    fix(layouts): place  to the end of the opening body tag
    The classes that were being applied to the app layout were not working because of the order that the attributes were settled in the body tag on the app layout.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,7 +12,9 @@
     <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
 
     {{-- Font Awesome --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="shortcut icon" href="{{ asset('favicon.svg') }}" type="image/x-icon">
 
@@ -21,7 +23,7 @@
     @stack('styles')
 </head>
 
-<body {{ $attributes }} x-cloak x-data="{ darkMode: $persist(false) }" :class="{ 'dark': darkMode === true }" class="antialiased">
+<body x-cloak x-data="{ darkMode: $persist(false) }" :class="{ 'dark': darkMode === true }" {{ $attributes }}>
     <x-navigation-bar />
 
     {{ $slot }}
