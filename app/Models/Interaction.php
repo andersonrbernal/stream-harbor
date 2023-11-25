6e3af478422b8c5fe1a92265c8210eb7ffa6f806
anderson <andersonbernal2017@gmail.com>
2023-11-25 19:05:32 -0400

    "feat: add countViews and countLikes methods to Interaction model"

--- app/Models/Interaction.php
+++ app/Models/Interaction.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Class Interaction
+ * 
+ * Represents an interaction between a user and a video.
+ * 
+ * @package App\Models
+ */
 class Interaction extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'liked',
         'viewed',
@@ -16,13 +29,47 @@ class Interaction extends Model
         'video_id',
     ];
 
-    public function user()
+    /**
+     * Get the user that the interaction belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function video()
+    /**
+     * Get the video that the interaction belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function video(): BelongsTo
     {
         return $this->belongsTo(Video::class);
+    }
+
+    /**
+     * Count the number of views for the video associated with this interaction.
+     *
+     * @return int
+     */
+    public function countViews(): int
+    {
+        return $this->where('video_id', $this->video_id)
+            ->where('viewed', true)
+            ->count();
+    }
+
+    /**
+     * Count the number of likes for the video associated with this interaction.
+     *
+     * @return int
+     */
+    public function countLikes(): int
+    {
+        return $this->where('video_id', $this->video_id)
+            ->where('liked', true)
+            ->count();
     }
 }
