5c3a5ad1b1f240f0d3d266b47f7b2435a96924c6
anderson <andersonbernal2017@gmail.com>
2023-11-25 16:00:09 -0400

    feat(models): add interaction model

---
+++ app/Models/Interaction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Interaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'liked',
+        'viewed',
+        'user_id',
+        'video_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function video()
+    {
+        return $this->belongsTo(Video::class);
+    }
+}
