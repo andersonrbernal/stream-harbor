48ff2c8cee01bb638dba73321afa8d213127d588
anderson <andersonbernal2017@gmail.com>
2023-12-01 20:28:33 -0400

    feat(lang): add english translations for trending video player component

---
+++ lang/en/components/livewire/trending-video-player.php
@@ -0,0 +1,11 @@
+<?php
+
+// /lang/en/components/livewire/trending-video-player.php
+
+return [
+    'video_not_supported' => 'Your browser does not support the video tag.',
+    'no_category' => 'There is no category for this video.',
+    'no_title' => 'There is no title for this video.',
+    'no_description' => 'There is no description for this video.',
+    "watch_now_button" => "Watch Now",
+];
