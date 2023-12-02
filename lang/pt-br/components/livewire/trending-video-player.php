c6c8eb6e40a881837dd01a9317c63293525faa44
anderson <andersonbernal2017@gmail.com>
2023-12-01 20:28:54 -0400

    feat(lang): add brazilian portuguese for trending video player component

---
+++ lang/pt-br/components/livewire/trending-video-player.php
@@ -0,0 +1,11 @@
+<?php
+
+// /lang/pt-br/components/livewire/trending-video-player.php
+
+return [
+    'video_not_supported' => 'Seu navegador não suporta a tag de vídeo.',
+    'no_category' => 'Não há categoria para este vídeo.',
+    'no_title' => 'Não há título para este vídeo.',
+    'no_description' => 'Não há descrição para este vídeo.',
+    "watch_now_button" => "Assista agora",
+];
