dfbbbddb26df0684c95b72a48f7f1b7e2dfc30e6
anderson <andersonbernal2017@gmail.com>
2023-12-10 13:11:42 -0400

    Add Portuguese translation for home page

---
+++ lang/pt-br/pages/home/index.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'home' => [
+        'index' => [
+            'title' => 'Home',
+            'trending_video' => 'Vídeo em alta',
+            'trending_videos' => 'Vídeos em alta',
+            'most_liked_videos' => 'Vídeos mais curtidos',
+            'most_viewed_videos' => 'Vídeos mais vistos',
+        ],
+    ],
+];
