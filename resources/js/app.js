bf5e441b7a1c9e3a6d568c897fd5785a2dad3fa7
anderson <andersonbernal2017@gmail.com>
2023-12-10 13:10:19 -0400

    Add Alpine.js plugin for intersection observer

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,12 @@ import 'flowbite';
 
 import Alpine from 'alpinejs'
 import persist from '@alpinejs/persist'
+import intersect from '@alpinejs/intersect'
 
 window.Alpine = Alpine
 
 Alpine.plugin(persist)
+Alpine.plugin(intersect)
 
 Alpine.start()
 
