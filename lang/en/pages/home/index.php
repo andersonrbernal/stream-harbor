7be69a678803cca234fa0efc9dc4d0c39aca641e
anderson <andersonbernal2017@gmail.com>
2023-12-10 13:11:31 -0400

    Add home page translations

---
+++ lang/en/pages/home/index.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'home' => [
+        'index' => [
+            'title' => 'Home',
+            'trending_video' => 'Trending Video',
+            'trending_videos' => 'Trending Videos',
+            'most_liked_videos' => 'Most Liked Videos',
+            'most_viewed_videos' => 'Most Viewed Videos',
+        ],
+    ],
+];
