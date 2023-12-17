65401a64c801ba4dbff21aad329befcd65148070
anderson <andersonbernal2017@gmail.com>
2023-12-16 20:05:40 -0400

    Add new methods to VideoRepositoryInterface

--- app/Repositories/Interfaces/VideoRepositoryInterface.php
+++ app/Repositories/Interfaces/VideoRepositoryInterface.php
@@ -11,4 +11,8 @@ interface VideoRepositoryInterface
     public function create(array $data);
     public function update($id, array $data);
     public function delete($id);
+    public function getTrendingVideo();
+    public function getTrendingVideos();
+    public function getMostLikedVideosWithPagination();
+    public function getMostViewedVideosWithPagination();
 }
