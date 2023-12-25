ce8f665b05c7c49ad1375f1f7792c755a8cf3c75
anderson <andersonbernal2017@gmail.com>
2023-12-25 19:40:38 -0400

    Add forgot password page brazilian portuguese translation

---
+++ lang/pt-br/pages/auth/forgot-password.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'title' => 'Recuperar senha',
+    'app_name' => 'Stream Harbor',
+    'attributes' => [
+        'email' => 'email',
+    ],
+    'email_input' => [
+        'placeholder' => 'Digite seu endereço de e-mail',
+        'label' => 'Endereço de e-mail',
+    ],
+    'buttons' => [
+        'submit' => 'Redefinir minha senha',
+    ],
+    'messages' => [
+        'success' => 'Enviamos seu link de redefinição de senha por e-mail!',
+        'error' => 'Algo deu errado! Por favor, tente novamente.',
+    ],
+    'form_description' => 'Digite seu endereço de e-mail e enviaremos um link para redefinir sua senha.',
+    'form_footer' => 'Lembrou sua senha?',
+    'form_footer_link' => 'Entrar',
+];
