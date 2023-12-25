c7de65ec368b6527918eebc922b282a5b620f0f9
anderson <andersonbernal2017@gmail.com>
2023-12-25 19:45:16 -0400

    Add user password reset email brazilian portuguese translation

---
+++ lang/pt-br/mails/user-password-reseted.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Sua senha foi redefinida',
+    'subject' => 'Senha redefinida.',
+    'presentation' => 'Olá :name,',
+    'message' => 'Sua senha foi redefinida recentemente. Se você não solicitou esta redefinição, tome medidas imediatas. Entre em contato com o suporte ou redefina sua senha.',
+    'description' => 'Por favor, faça login com sua nova senha.',
+    'go_to_login_link' => 'Ir para o Login',
+    'regards' => 'Atenciosamente, :team',
+];
