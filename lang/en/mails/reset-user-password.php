e57e307536017c0079583455795701bb21e31e89
anderson <andersonbernal2017@gmail.com>
2023-12-25 19:43:53 -0400

    Add reset user password email english translations

---
+++ lang/en/mails/reset-user-password.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'subject' => 'Reset your password',
+    'title' => 'Reset your password',
+    'presentation' => 'Hello :name,',
+    'message' => 'You are receiving this email because we received a password reset request for your account.',
+    'token_expiration' => 'This password reset link will expire in :count minutes.',
+    'button' => 'Reset Password',
+    'if_youre_having_trouble_clicking_the_button' => 'If you’re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser:',
+    'copy_and_paste_url' => 'Copy and paste the URL below into your web browser:',
+    'your_requested_a_password_reset_to_email' => 'You requested a password reset to :email. Click the button below to reset your password.',
+    'not_requested_password_reset' => 'If you did not request a password reset, no further action is required.',
+    'footer' => 'If you did not request a password reset, no further action is required.',
+    'regards' => 'Regards, :team Team',
+];
