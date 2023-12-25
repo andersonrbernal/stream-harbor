9638a0d00faff84804cc6dac416984abc0865129
anderson <andersonbernal2017@gmail.com>
2023-12-25 19:46:00 -0400

    Refactor email routes to use resetPassword method

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,7 @@ Route::group(["prefix" => "{locale}"], function () {
         Route::get('/register', [AuthenticationController::class, 'register'])->name('register');
         Route::get('/login', [AuthenticationController::class, 'login'])->name('login');
         Route::get('/forgot-password', [AuthenticationController::class, 'forgotPassword'])->name('forgot-password');
-    });
-
-    // Emails
-    Route::group(['prefix' => 'emails', 'as' => 'emails.'], function () {
-        Route::get('/verify', fn () => view('pages.emails.verify', ['title' => 'Verify your email']))->name('verify');
-        Route::get('/reset', fn () => view('pages.emails.reset', ['title' => 'Reset your password']))->name('reset');
+        Route::get('/reset-password/{token}', [AuthenticationController::class, 'resetPassword'])->name('reset-password');
     });
 
     // Videos
