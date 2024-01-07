856dc24945d5f05fb96dacf87e963fd0707a943c
anderson <andersonbernal2017@gmail.com>
2024-01-07 19:49:44 -0400

    Add Portuguese translation for update profile user component

---
+++ lang/pt-br/components/livewire/update-profile-user.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'title' => 'Perfil',
+    'name' => 'Nome',
+    'email' => 'Email',
+    'photo' => 'Foto',
+    'country' => 'País',
+    'profile_data_share_warning' => 'Essas informações serão exibidas publicamente, então tenha cuidado com o que você compartilha.',
+    'update_profile_button' => 'Atualizar Perfil',
+    'attributes' => [
+        'name' => 'nome',
+        'email' => 'email',
+        'profile_photo' => 'foto',
+        'country_id' => 'país',
+    ],
+    'name_input' => [
+        'label' => 'Nome',
+        'placeholder' => 'Digite seu nome',
+    ],
+    'email_input' => [
+        'label' => 'Email',
+        'placeholder' => 'Digite seu email',
+    ],
+    'country_select' => [
+        'label' => 'País',
+        'placeholder' => 'Selecione seu país',
+    ],
+    'profile_photo_input' => [
+        'label' => 'Foto',
+        'placeholder' => 'Selecione sua foto',
+    ],
+    'buttons' => [
+        'update' => 'Atualizar',
+    ],
+    'messages' => [
+        'update' => 'Perfil atualizado com sucesso.',
+        'error' => 'Erro ao atualizar perfil do usuário.',
+    ],
+];
